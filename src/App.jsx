698902136e8231cc2c1744abdb4f396433253ad4
VISHAL SAHOO <bhagis052@gmail.com>
2024-09-14 16:05:58 +0530

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Navbar from './components/Navbar'; // Add this import at the top of the file
 import { TypeWriter } from './components/TypeWriter';
 import Footer from './components/Footer';
+import KeyGenerator from './components/Keygenerator';
 function App() {
   
 
@@ -10,12 +11,13 @@ function App() {
       <Navbar />
       <div className="home-content">
         <h1 className="main-title">
-          Master and unlock the power of <span className="highlight"><TypeWriter/></span>
+          Master and Unlock the power of<p className="highlight"><TypeWriter/></p>
         </h1>
         <div className="input-container">
           <button className="create-wallet-btn">Connect Wallet</button>
         </div>
       </div>
+      <KeyGenerator />
       <Footer />
     </div>
   )
--- src/components/TypeWriter.jsx
+++ src/components/TypeWriter.jsx
@@ -8,7 +8,7 @@ export const TypeWriter = () => {
     const [wordIndex, setWordIndex] = useState(0);
     const [typingSpeed, setTypingSpeed] = useState(150);
   
-    const words = ['Wallets','Transactions', 'Crypto', 'WEB3', 'Payments', 'Hashing'];
+    const words = ['Wallets', 'Crypto', 'WEB3', 'Payments', 'Hashing','NFTs','Transactions'];
   
     useEffect(() => {
       const handleTyping = () => {
